aa7f659fdc595a666cb6de9576dc1d5de651f642
Saugaat <saugaatallabadi@gmail.com>
2020-03-28 13:16:30 +0400

    First blank rest project

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Sallabadi002/Documents/MyProjects/Python_All_Courses/Django_Projects/DjangoRest_AaravTech/quiz/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
